552f1ba6a717fd3cadac1c5d6b74131c079181af
Zach Dennis <zach.dennis@gmail.com>
2013-12-12 14:50:21 -0500

    adding ios support for phonegap app: phonegap build ios

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+]
+});
