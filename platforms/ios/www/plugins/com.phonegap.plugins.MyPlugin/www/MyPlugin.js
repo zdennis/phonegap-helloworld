10551b660b1fe66eb5a8de7e2fc10a3b27b3b859
Zach Dennis <zach.dennis@gmail.com>
2014-01-07 13:09:43 -0500

    Removing and re-adding the plugin.

--- platforms/ios/www/plugins/com.phonegap.plugins.MyPlugin/www/MyPlugin.js
+++ platforms/ios/www/plugins/com.phonegap.plugins.MyPlugin/www/MyPlugin.js
@@ -12,11 +12,11 @@ cordova.define("com.phonegap.plugins.MyPlugin.MyPlugin", function(require, expor
   },
 
   GeLoBeacon: function(beacon) {
-    this.beaconId = beacon.beaconId,
-    this.signalStrength = beacon.signalStrength,
-    this.receivedRSSI = beacon.receivedRSSI,
-    this.timeToLive = beacon.timeToLive,
-    this.txPower = beacon.txPower
+    this.beaconId = beacon.beaconId;
+    this.signalStrength = beacon.signalStrength;
+    this.receivedRSSI = beacon.receivedRSSI;
+    this.timeToLive = beacon.timeToLive;
+    this.txPower = beacon.txPower;
   },
 
   on: function(sdkConstant, callback){
